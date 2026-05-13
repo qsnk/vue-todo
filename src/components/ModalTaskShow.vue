4306a8255b4f76c1ad50546f70fe6b57cf72f7f7
qsnk <mikhailroot01@gmail.com>
2026-05-13 22:20:00 +0300

    Инит. Карточки in-memory, просмотр и удаление

---
+++ src/components/ModalTaskShow.vue
@@ -0,0 +1,37 @@
+<template>
+  <Teleport to="body">
+    <div v-if="show" class="fixed inset-0 z-50 flex items-center justify-center p-4 font-mono">
+      <div class="fixed inset-0 bg-black/50" @click="$emit('close')"></div>
+
+      <div class="relative bg-white rounded-lg shadow-xl max-w-md w-full p-6">
+        <h3 class="text-lg text-center font-bold mb-4">
+          <slot name="header">Заголовок по умолчанию</slot>
+        </h3>
+
+        <div class="mb-6">
+          <div class="flex flex-col my-4">
+            <label class="text-xl font-semibold">Название</label>
+            <slot name="title">Заголовок по умолчанию</slot>
+          </div>
+          <div class="flex flex-col my-4">
+            <label class="text-xl font-semibold">Описание</label>
+            <slot name="description">Отсутствует</slot>
+          </div>
+        </div>
+        <div class="flex gap-2">
+          <button
+            @click="$emit('close')"
+            class="w-full bg-red-600 text-white py-2 rounded-md hover:bg-red-700 transition"
+          >
+            Закрыть
+          </button>
+        </div>
+      </div>
+    </div>
+  </Teleport>
+</template>
+
+<script setup lang="ts">
+defineProps(['show'])
+defineEmits(['close'])
+</script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -13,46 +13,56 @@
     </button>
   </div>
   <div v-if="!isTodoEmpty">
-    <div class="flex gap-2">
-      <span class="flex text-center">Всего задач: {{ todos.length }}</span>
+    <div class="flex p-2 gap-2">
+      <span class="flex text-center items-center">Всего задач: {{ todos.length }}</span>
       <button
         @click="isModalOpen = true"
         class="w-10 h-10 p-2 border border-blue-600 rounded-md bg-white text-blue-600 hover:bg-blue-700 hover:text-white transition"
       >
-        +
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg>
       </button>
-      <Modal :show="isModalOpen" @close="isModalOpen = false" @save="handleSave">
+      <ModalAdd :show="isModalOpen" @close="isModalOpen = false" @save="handleSave">
         <template #header>Добавление задачи</template>
-      </Modal>
+      </ModalAdd>
     </div>
 
+    <ModalShow :show="!!selectedItem" @close="selectedItem = null">
+      <template #header>Задача</template>
+      <template #title>{{ selectedItem?.title }}</template>
+      <template v-if="selectedItem?.has_description" #description>{{ selectedItem?.description }}</template>
+    </ModalShow>
+    
     <div class="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-1">
       <div
         v-for="item in todos"
         :key="item.id"
-        class="flex mx-4 my-2 p-4 rounded-md justify-between bg-gray-200 border border-blue-500 shadow-sm"
+        class="flex flex-col gap-2 mx-4 my-2 p-4 rounded-md justify-between bg-gray-100  hover:bg-blue-500 hover:text-white transition shadow-sm"
       >
-        <p class="flex text-sm items-center text-center">Title: {{ item.title }}</p>
-        <p class="flex text-sm items-center text-center">id: {{ item.id }}</p>
-        <div class="flex gap-1">
+        <div class="flex justify-between gap-4">
+          <p class="text-sm items-center text-left truncate">{{ item.title }}</p>
+          <p class="text-sm font-extralight text-center shrink-0 rounded-full bg-blue-400 text-white w-5 h-5">{{ item.id }}</p>
+        </div>
+        <div class="flex mx-2 justify-center gap-1">
           <button
+            @click="selectedItem = item"
             type="button"
             class="p-2 rounded-md bg-blue-600 text-white hover:bg-blue-700 transition"
           >
-            Show
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M120-120v-320h80v184l504-504H520v-80h320v320h-80v-184L256-200h184v80H120Z"/></svg>
+
           </button>
           <button
             type="button"
             class="p-2 rounded-md bg-blue-600 text-white hover:bg-blue-700 transition"
           >
-            Edit
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>
           </button>
           <button
             @click="removeTask(item.id)"
             type="button"
             class="p-2 rounded-md bg-red-600 text-white hover:bg-red-700 transition"
           >
-            Delete
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
           </button>
         </div>
       </div>
@@ -66,15 +76,16 @@
     >
       Добавить?
     </button>
-    <Modal :show="isModalOpen" @close="isModalOpen = false" @save="handleSave">
+    <ModalAdd :show="isModalOpen" @close="isModalOpen = false" @save="handleSave">
       <template #header>Добавление задачи</template>
-    </Modal>
+    </ModalAdd>
   </div>
 </template>
 
 <script setup lang="ts">
 import { computed, ref } from 'vue'
-import Modal from '@/components/ModalTaskAdd.vue'
+import ModalAdd from '@/components/ModalTaskAdd.vue'
+import ModalShow from '@/components/ModalTaskShow.vue'
 
 interface TodoItem {
   title: string
@@ -82,17 +93,22 @@ interface TodoItem {
 }
 
 const isModalOpen = ref<boolean>(false)
+
 const todos = ref<Array<object>>([])
-const searchTerm = ref<string>('')
 const searchedTodos = ref<Array<object>>([])
+
 const isTodoEmpty = computed(() => todos.value.length === 0)
 const isSearchedTodoEmpty = computed(() => searchedTodos.value.length === 0)
+
+const searchTerm = ref<string>('')
+const selectedItem = ref<any>(null);
 
 const handleSave = (item: TodoItem) => {
   todos.value.push({
     id: todos.value.length + 1,
     title: item.title.value,
     description: item.description.value,
+    has_description: item.description.value.length > 0 ? true : false
   })
   isModalOpen.value = false
 }
